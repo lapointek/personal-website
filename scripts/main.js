c677f69cae5ce49305ea144e44083a76551f3adc
lapointek <lapointek.dev@gmail.com>
2026-01-30 17:11:13 -0500

    revert: javascript and projects html code

--- scripts/main.js
+++ scripts/main.js
@@ -5,16 +5,3 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navmenu.classList.toggle("active");
 });
-
-document.querySelectorAll(".content").forEach((div) => {
-  const video = div.querySelector("video");
-  const link = div.dataset.link;
-
-  div.addEventListener("click", (e) => {
-    if (video.paused) {
-      video.play;
-    } else {
-      window.open(link, "_blank");
-    }
-  });
-});
