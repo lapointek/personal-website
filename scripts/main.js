fe6fcbc77c6f289b4b92d5444521eeafbe3d0443
lapointek <lapointek.dev@gmail.com>
2026-01-30 17:03:59 -0500

    chore: add javascript to check if video is playing

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,16 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navmenu.classList.toggle("active");
 });
+
+document.querySelectorAll(".content").forEach((div) => {
+  const video = div.querySelector("video");
+  const link = div.dataset.link;
+
+  div.addEventListener("click", (e) => {
+    if (video.paused) {
+      video.play;
+    } else {
+      window.open(link, "_blank");
+    }
+  });
+});
